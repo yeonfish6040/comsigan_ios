f08df2fd33c368dfcbfbcbf7c9b46c349ea6ad54
yeonfish6040 <yeonfish6040@gmail.com>
2026-08-13 08:32:22 +0900

    Initial Commit

---
+++ comsigan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  comsigan
+//
+//  Created by Yeonjun Lee on 8/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ comsigan/comsiganApp.swift
@@ -0,0 +1,17 @@
+//
+//  comsiganApp.swift
+//  comsigan
+//
+//  Created by Yeonjun Lee on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct comsiganApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
